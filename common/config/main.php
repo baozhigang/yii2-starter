8946da52ad15a0573c307518d3ccb92dd255c78d
“baozhigang” <646637969@qq.com>
2022-02-03 21:16:28 +0800

    使用 Yarn 来管理前端依赖

--- common/config/main.php
+++ common/config/main.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'aliases' => [
-        '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
+        '@bower' => '@node_modules',
+        '@npm'   => '@node_modules',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
